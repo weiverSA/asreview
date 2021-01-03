e9cd7f3412f16566bf09809f1c60c28dce083bdd
Jonathan de Bruin <jonathandebruinos@gmail.com>
2021-01-03 13:52:33 +0100

    Add option to select Oracle, Explore, and Simulate project types on project creation (#449)

--- asreview/webapp/src/PreReviewComponents/ProjectInit.js
+++ asreview/webapp/src/PreReviewComponents/ProjectInit.js
@@ -8,11 +8,6 @@ import {
   DialogContent,
   DialogActions,
   Dialog,
-  List,
-  ListItem,
-  ListItemSecondaryAction,
-  ListItemText,
-  Switch,
 } from '@material-ui/core'
 
 import { brown } from '@material-ui/core/colors';
@@ -23,6 +18,8 @@ import { setProject } from '../redux/actions'
 
 import { connect } from "react-redux";
 import { api_url, mapStateToProps, projectModes } from '../globals.js';
+import ProjectModeSelect from './ProjectModeSelect';
+
 
 import './ReviewZone.css';
 
@@ -42,8 +39,7 @@ const useStyles = makeStyles(theme => ({
     backgroundColor: theme.palette.warning.light
   },
   textfieldItem: {
-    marginTop: 0,
-    marginBottom: 40,
+    marginBottom: theme.spacing(3),
   },
   clear: {
     overflow: "auto",
@@ -85,14 +81,22 @@ const ProjectInit = (props) => {
   })
   const [error, setError] = React.useState(false)
 
-  const toggleMode = () => {
+  // const toggleMode = () => {
+  //   setInfo({
+  //     ...info,
+  //     mode: info.mode === projectModes.ORACLE ? projectModes.SIMULATION : projectModes.ORACLE
+  //   });
+  // };
+
+  // handle project type/mode change
+  const onModeChange = (event) => {
     setInfo({
       ...info,
-      mode: info.mode === projectModes.ORACLE ? projectModes.SIMULATION : projectModes.ORACLE
+      mode: event.target.value
     });
   };
 
-  const onChange = (evt) => { 
+  const onChange = (evt) => {
     setInfo({
       ...info,
       [evt.target.name]: evt.target.value
@@ -143,8 +147,16 @@ const ProjectInit = (props) => {
       <form noValidate autoComplete="off">
 
         <div className={classes.textfieldItem}>
+          <ProjectModeSelect
+            mode={info.mode}
+            onModeChange={onModeChange}
+          />
+        </div>
+
+        <div className={classes.textfieldItem}>
           <TextField
             fullWidth
+            variant="outlined"
             error={error}
             autoFocus={true}
             required
@@ -160,6 +172,7 @@ const ProjectInit = (props) => {
         <div className={classes.textfieldItem}>
           <TextField
             fullWidth
+            variant="outlined"
             name="authors"
             id="project-author"
             label="Your name"
@@ -171,6 +184,7 @@ const ProjectInit = (props) => {
         <div className={classes.textfieldItem}>
           <TextField
             fullWidth
+            variant="outlined"
             multiline
             rows={4}
             rowsMax={6}
@@ -181,19 +195,6 @@ const ProjectInit = (props) => {
             value={info.description}
           />
         </div>
-
-        <List className={classes.list}>
-            <ListItem>
-              <ListItemText id="switch-list-label-simulation" primary="Simulation" secondary="Some explanation about simulation mode.."/>
-              <ListItemSecondaryAction>
-                <Switch
-                  edge="end"
-                  onChange={toggleMode}
-                />
-              </ListItemSecondaryAction>
-            </ListItem>
-         </List>   
-
       </form>
       </DialogContent>
       <DialogActions>
